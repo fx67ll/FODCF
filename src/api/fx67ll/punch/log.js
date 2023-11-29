7ba9d12affdb2cec2d63dc27366bb87c7525705e
fx67ll <2717745014@qq.com>
2023-11-29 17:54:15 +0800

    add punch log

---
+++ src/api/fx67ll/punch/log.js
@@ -0,0 +1,44 @@
+import request from "@/utils/request";
+
+// 查询打卡记录列表
+export function listLog(query) {
+  return request({
+    url: "/punch/log/list",
+    method: "get",
+    params: query,
+  });
+}
+
+// 查询打卡记录详细
+export function getLog(punchId) {
+  return request({
+    url: "/punch/log/" + punchId,
+    method: "get",
+  });
+}
+
+// 新增打卡记录
+export function addLog(data) {
+  return request({
+    url: "/punch/log",
+    method: "post",
+    data: data,
+  });
+}
+
+// 修改打卡记录
+export function updateLog(data) {
+  return request({
+    url: "/punch/log",
+    method: "put",
+    data: data,
+  });
+}
+
+// 删除打卡记录
+export function delLog(punchId) {
+  return request({
+    url: "/punch/log/" + punchId,
+    method: "delete",
+  });
+}
---
+++ src/views/punch/log/index.vue
@@ -0,0 +1,412 @@
+<template>
+  <div class="app-container">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
+      <el-form-item label="打卡类型" prop="punchType">
+        <el-select v-model="queryParams.punchType" placeholder="请选择打卡类型" clearable>
+          <el-option
+            v-for="dict in dict.type.fx67ll_punch_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <!-- <el-form-item label="删除标志" prop="delFlag">
+        <el-select v-model="queryParams.delFlag" placeholder="请选择删除标志" clearable>
+          <el-option
+            v-for="dict in dict.type.sys_yes_no"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item> -->
+      <!-- <el-form-item label="用户ID" prop="userId">
+        <el-input
+          v-model="queryParams.userId"
+          placeholder="请输入用户ID"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item> -->
+      <el-form-item label="创建者" prop="createBy">
+        <el-input
+          v-model="queryParams.createBy"
+          placeholder="请输入记录创建者"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="创建时间">
+        <el-date-picker
+          v-model="daterangeCreateTime"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <!-- <el-form-item label="记录更新者" prop="updateBy">
+        <el-input
+          v-model="queryParams.updateBy"
+          placeholder="请输入记录更新者"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item> -->
+      <el-form-item label="更新时间">
+        <el-date-picker
+          v-model="daterangeUpdateTime"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery"
+          >搜索</el-button
+        >
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['punch:log:add']"
+          >新增</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['punch:log:edit']"
+          >修改</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['punch:log:remove']"
+          >删除</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['punch:log:export']"
+          >导出</el-button
+        >
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table
+      v-loading="loading"
+      :data="logList"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55" align="center" />
+      <!-- <el-table-column label="打卡记录主键" align="center" prop="punchId" /> -->
+      <el-table-column label="打卡类型" align="center" prop="punchType">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.fx67ll_punch_type" :value="scope.row.punchType" />
+        </template>
+      </el-table-column>
+      <el-table-column label="记录创建者" align="center" prop="createBy" />
+      <el-table-column label="记录创建时间" align="center" prop="createTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime, "{y}-{m}-{d}") }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="记录更新者" align="center" prop="updateBy" />
+      <el-table-column label="记录更新时间" align="center" prop="updateTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.updateTime, "{y}-{m}-{d}") }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['punch:log:edit']"
+            >修改</el-button
+          >
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['punch:log:remove']"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改打卡记录对话框 -->
+    <el-dialog
+      :title="title"
+      :visible.sync="open"
+      width="500px"
+      style="top: 200px"
+      append-to-body
+    >
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="打卡类型" prop="punchType">
+          <el-select
+            v-model="form.punchType"
+            style="width: 100%"
+            placeholder="请选择打卡类型"
+          >
+            <el-option
+              v-for="dict in dict.type.fx67ll_punch_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <!-- <el-form-item label="删除标志" prop="delFlag">
+          <el-select v-model="form.delFlag" placeholder="请选择删除标志">
+            <el-option
+              v-for="dict in dict.type.sys_yes_no"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item> -->
+        <!-- <el-form-item label="用户ID" prop="userId">
+          <el-input v-model="form.userId" placeholder="请输入用户ID" />
+        </el-form-item> -->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listLog, getLog, delLog, addLog, updateLog } from "@/api/fx67ll/punch/log";
+
+export default {
+  name: "Log",
+  dicts: ["sys_yes_no", "fx67ll_punch_type"],
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 打卡记录表格数据
+      logList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 用户ID时间范围
+      daterangeCreateTime: [],
+      // 用户ID时间范围
+      daterangeUpdateTime: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        punchType: null,
+        delFlag: null,
+        userId: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        punchType: [{ required: true, message: "打卡类型不能为空", trigger: "change" }],
+      },
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    // 重置时间段查询
+    clearDateQueryParams() {
+      this.queryParams.beginCreateTime = null;
+      this.queryParams.endCreateTime = null;
+      this.queryParams.beginUpdateTime = null;
+      this.queryParams.endUpdateTime = null;
+    },
+    /** 查询打卡记录列表 */
+    getList() {
+      this.loading = true;
+      this.clearDateQueryParams();
+      if (null != this.daterangeCreateTime && "" != this.daterangeCreateTime) {
+        this.queryParams.beginCreateTime = this.daterangeCreateTime[0];
+        this.queryParams.endCreateTime = this.daterangeCreateTime[1];
+      }
+      if (null != this.daterangeUpdateTime && "" != this.daterangeUpdateTime) {
+        this.queryParams.beginUpdateTime = this.daterangeUpdateTime[0];
+        this.queryParams.endUpdateTime = this.daterangeUpdateTime[1];
+      }
+      listLog(this.queryParams).then((response) => {
+        this.logList = this.formatObjectArrayNullProperty(response.rows);
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        punchId: null,
+        punchType: null,
+        delFlag: null,
+        userId: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.daterangeCreateTime = [];
+      this.daterangeUpdateTime = [];
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map((item) => item.punchId);
+      this.single = selection.length !== 1;
+      this.multiple = !selection.length;
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加打卡记录";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const punchId = row.punchId || this.ids;
+      getLog(punchId).then((response) => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改打卡记录";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate((valid) => {
+        if (valid) {
+          if (this.form.punchId != null) {
+            updateLog(this.form).then((response) => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addLog(this.form).then((response) => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const punchIds = row.punchId || this.ids;
+      this.$modal
+        .confirm('是否确认删除打卡记录编号为"' + punchIds + '"的数据项？')
+        .then(function () {
+          return delLog(punchIds);
+        })
+        .then(() => {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        })
+        .catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download(
+        "punch/log/export",
+        {
+          ...this.queryParams,
+        },
+        `log_${new Date().getTime()}.xlsx`
+      );
+    },
+  },
+};
+</script>
