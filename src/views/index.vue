c0e996b6060a469663ae3b9af24f4686263d9cef
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-03-11 16:48:20 +0800

    optimize & add egg

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,13 +3,12 @@
     <el-row :gutter="20">
       <!-- 左侧：品牌与Logo -->
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>fx67ll's 后台管理框架</h2>
+        <h2>fx67ll's 后台管理框架 <span class="fx67ll-version" @click="showFirstJobDate()">v{{ version }}</span></h2>
         <el-image src="https://test.fx67ll.com/fx67ll-img-collection/fx67ll.jpg"
           :preview-src-list="['https://test.fx67ll.com/fx67ll-img-collection/fx67ll.jpg']"
           style="margin-top: 12px; display: block; border-radius: 4px; width: 123px; height:123px;"
           fit="cover"></el-image>
       </el-col>
-
       <!-- 右侧：技术选型 -->
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
         <el-row>
@@ -45,28 +44,130 @@
         </el-row>
       </el-col>
     </el-row>
+
     <el-divider />
+
+    <el-row :gutter="24" v-show="isShowFirstJobDate">
+      <div class="fx67ll-clock"></div>
+      <div class="fx67ll-clock-desc">Code is my life ＼＼\٩('ω')و//／／ Since {{ firstJobDate }}</div>
+    </el-row>
   </div>
 </template>
 
 <script>
+import { version as packageVersion } from '../../package.json';
+import FlipClock from 'flipclock';
+import 'flipclock/dist/flipclock.css';
+import moment from 'moment';
 export default {
   name: "Index",
   data() {
     return {
-      // version: "3.8.5",
+      version: packageVersion,
+      isShowFirstJobDate: false,
+      firstJobDate: '2018-05-28',
+      firstJobDays: 0,
+      chnNumChar: ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'],
+      chnUnitSection: ['', '万', '亿', '万亿', '亿亿'],
+      chnUnitChar: ['', '十', '百', '千'],
     };
+  },
+  mounted() {
+    this.initStuDays();
+    this.initClock();
   },
   methods: {
     goTarget(href) {
       window.open(href, "_blank");
     },
-  },
+    showFirstJobDate() {
+      this.isShowFirstJobDate = !this.isShowFirstJobDate;
+    },
+    // 天数计算
+    initStuDays() {
+      let firstJobDays = moment(moment().format('YYYY-MM-DD')).diff(moment(this.firstJobDate).format('YYYY-MM-DD'), 'day');
+      this.firstJobDays = this.NumberToChinese(firstJobDays);
+    },
+    // 时钟初始化
+    initClock() {
+      const el = document.querySelector('.fx67ll-clock');
+      const clock = new FlipClock(el, new Date(this.firstJobDate), {
+        face: 'DayCounter', // 类型
+        showSeconds: true, // 显示秒数
+        showLabels: true, // 显示文字标识
+        language: {
+          years: '年',
+          months: '月',
+          days: '日',
+          hours: '時',
+          minutes: '分',
+          seconds: '秒'
+        },
+        countdown: false // true为倒计时
+      });
+    },
+    // 阿拉伯数字转换汉字数字
+    NumberToChinese(num) {
+      var unitPos = 0;
+      var strIns = '',
+        chnStr = '';
+      var needZero = false;
+
+      if (num === 0) {
+        return this.chnNumChar[0];
+      }
+
+      while (num > 0) {
+        var section = num % 10000;
+        if (needZero) {
+          chnStr = this.chnNumChar[0] + chnStr;
+        }
+        strIns = this.SectionToChinese(section);
+        strIns += section !== 0 ? this.chnUnitSection[unitPos] : this.chnUnitSection[0];
+        chnStr = strIns + chnStr;
+        needZero = section < 1000 && section > 0;
+        num = Math.floor(num / 10000);
+        unitPos++;
+      }
+
+      if (chnStr.substring(0, 1) === '一' && chnStr.substring(1, 2) === '十') {
+        chnStr = chnStr.substr(1, chnStr.length);
+      }
+
+      return chnStr;
+    },
+    // 节内转换算法
+    SectionToChinese(section) {
+      var strIns = '',
+        chnStr = '';
+      var unitPos = 0;
+      var zero = true;
+      while (section > 0) {
+        var v = section % 10;
+        if (v === 0) {
+          if (!zero) {
+            zero = true;
+            chnStr = this.chnNumChar[v] + chnStr;
+          }
+        } else {
+          zero = false;
+          strIns = this.chnNumChar[v];
+          strIns += this.chnUnitChar[unitPos];
+          chnStr = strIns + chnStr;
+        }
+        unitPos++;
+        section = Math.floor(section / 10);
+      }
+      return chnStr;
+    },
+  }
 };
 </script>
 
 <style scoped lang="scss">
 .home {
+  cursor: pointer;
+
   blockquote {
     padding: 10px 20px;
     margin: 0 0 20px;
@@ -132,5 +233,24 @@ export default {
       padding-inline-start: 40px;
     }
   }
+}
+
+.fx67ll-version {
+  font-size: 16px;
+
+  &:hover {
+    color: #2ECC71;
+  }
+}
+
+.fx67ll-clock {
+  margin-left: 20px;
+  margin-top: 30px;
+}
+
+.fx67ll-clock-desc {
+  font-size: 18px;
+  margin-left: 20px;
+  margin-top: 30px;
 }
 </style>
