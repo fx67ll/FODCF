91cce17cec0523706cb02d6a356231bfc377b5f4
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-03-13 17:44:22 +0800

    finish optimize

--- src/views/fx67ll/dortmund/information/match/index.vue
+++ src/views/fx67ll/dortmund/information/match/index.vue
@@ -1,45 +1,58 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="比赛唯一业务编码" prop="matchCode">
-        <el-input v-model="queryParams.matchCode" placeholder="请输入比赛唯一业务编码" clearable
-          @keyup.enter.native="handleQuery" />
+      <el-form-item label="比赛编码" prop="matchCode" v-if="isMoreQuery">
+        <el-input v-model="queryParams.matchCode" placeholder="请输入比赛编码" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
-      <el-form-item label="所属赛季ID" prop="seasonId">
-        <el-input v-model="queryParams.seasonId" placeholder="请输入所属赛季ID" clearable @keyup.enter.native="handleQuery" />
+      <el-form-item label="所属赛季" prop="seasonId" v-if="isMoreQuery">
+        <common-enhanced-select ref="seasonSelect" v-model="queryParams.seasonId" valueKey="seasonId"
+          labelKey="seasonName" :api-func="listSeason" placeholder="请选择所属赛季" :enter-callback="handleQuery" />
       </el-form-item>
-      <el-form-item label="主队球队ID" prop="homeTeamId">
-        <el-input v-model="queryParams.homeTeamId" placeholder="请输入主队球队ID" clearable
-          @keyup.enter.native="handleQuery" />
+      <el-form-item label="主队名称" prop="homeTeamId">
+        <common-enhanced-select ref="homeTeamSelect" v-model="queryParams.homeTeamId" valueKey="teamId"
+          labelKey="teamName" :api-func="listTeam" placeholder="请选择主队名称" :enter-callback="handleQuery" />
       </el-form-item>
-      <el-form-item label="客队球队ID" prop="awayTeamId">
-        <el-input v-model="queryParams.awayTeamId" placeholder="请输入客队球队ID" clearable
-          @keyup.enter.native="handleQuery" />
+      <el-form-item label="客队名称" prop="awayTeamId">
+        <common-enhanced-select ref="awayTeamSelect" v-model="queryParams.awayTeamId" valueKey="teamId"
+          labelKey="teamName" :api-func="listTeam" placeholder="请选择客队名称" :enter-callback="handleQuery" />
       </el-form-item>
-      <el-form-item label="比赛开球时间" prop="matchTime">
-        <el-date-picker clearable v-model="queryParams.matchTime" type="date" value-format="yyyy-MM-dd"
-          placeholder="请选择比赛开球时间">
-        </el-date-picker>
+      <el-form-item label="比赛时间" prop="matchTime">
+        <el-date-picker v-model="daterangeMatchTime" style="width: 240px" value-format="yyyy-MM-dd" type="daterange"
+          range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" clearable></el-date-picker>
       </el-form-item>
-      <el-form-item label="比赛举办场地名称" prop="matchVenue">
-        <el-input v-model="queryParams.matchVenue" placeholder="请输入比赛举办场地名称" clearable
-          @keyup.enter.native="handleQuery" />
+      <el-form-item label="比赛场地" prop="matchVenue" v-if="isMoreQuery">
+        <el-select v-model="queryParams.matchVenue" style="width: 100%" placeholder="请选择或输入比赛场地" clearable filterable
+          allow-create default-first-option>
+          <el-option v-for="item in teamVenueOptions" :key="item.value" :label="item.label" :value="item.value"
+            @keyup.enter.native="handleQuery" />
+        </el-select>
       </el-form-item>
-      <el-form-item label="AI分析次数" prop="analysisCount">
-        <el-input v-model="queryParams.analysisCount" placeholder="请输入AI分析次数" clearable
-          @keyup.enter.native="handleQuery" />
+      <el-form-item label="比赛状态" prop="matchStatus" v-if="isMoreQuery">
+        <el-select v-model="queryParams.matchStatus" style="width: 100%" placeholder="请选择比赛状态" clearable
+          @keyup.enter.native="handleQuery">
+          <el-option v-for="dict in dict.type.sys_notice_status" :key="dict.value" :label="dict.label"
+            :value="dict.value"></el-option>
+        </el-select>
       </el-form-item>
-      <el-form-item label="创建时间">
+      <el-form-item label="分析次数" prop="analysisCount" v-if="isMoreQuery">
+        <el-input v-model="queryParams.analysisCount" type="number" min="0" max="1023" step="1" placeholder="请输入分析次数"
+          clearable @keyup.enter.native="handleQuery" />
+      </el-form-item>
+      <el-form-item label="创建时间" v-if="isMoreQuery">
         <el-date-picker v-model="daterangeCreateTime" style="width: 240px" value-format="yyyy-MM-dd" type="daterange"
           range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" clearable></el-date-picker>
       </el-form-item>
-      <el-form-item label="更新时间">
+      <el-form-item label="更新时间" v-if="isMoreQuery">
         <el-date-picker v-model="daterangeUpdateTime" style="width: 240px" value-format="yyyy-MM-dd" type="daterange"
           range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" clearable></el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button type="info" :icon="isMoreQuery ? 'el-icon-zoom-out' : 'el-icon-zoom-in'" size="mini"
+          @click="handleMoreQuery">
+          {{ isMoreQuery ? "关闭高级搜索" : "使用高级搜索" }}
+        </el-button>
       </el-form-item>
     </el-form>
 
@@ -65,33 +78,41 @@
 
     <el-table v-loading="loading" :data="matchList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="比赛唯一标识" align="center" prop="matchId" />
-      <el-table-column label="比赛唯一业务编码" align="center" prop="matchCode" />
-      <el-table-column label="所属赛季ID" align="center" prop="seasonId" />
-      <el-table-column label="主队球队ID" align="center" prop="homeTeamId" />
-      <el-table-column label="客队球队ID" align="center" prop="awayTeamId" />
-      <el-table-column label="比赛开球时间" align="center" prop="matchTime" width="180">
+      <el-table-column label="比赛编码" align="center" width="100">
+        <template #default="scope">
+          {{
+            scope.row.matchCode
+              ? (scope.row.matchCode.length > 9 ? scope.row.matchCode.slice(-9) : scope.row.matchCode)
+              : '-'
+          }}
+        </template>
+      </el-table-column>
+      <el-table-column label="所属赛季" align="center" prop="seasonName" width="100" />
+      <el-table-column label="主队名称" align="center" prop="homeTeamName" width="200" />
+      <el-table-column label="客队名称" align="center" prop="awayTeamName" width="200" />
+      <el-table-column label="比赛时间" align="center" prop="matchTime" width="100">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.matchTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="比赛举办场地名称" align="center" prop="matchVenue" />
-      <el-table-column label="比赛业务备注" align="center" prop="matchRemark" />
-      <el-table-column label="比赛状态" align="center" prop="matchStatus" />
-      <el-table-column label="AI分析次数" align="center" prop="analysisCount" />
+      <el-table-column label="比赛场地" align="center" prop="matchVenue" width="180" />
+      <el-table-column label="比赛状态" align="center" prop="matchStatus" width="80">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.sys_notice_status" :value="scope.row.matchStatus" />
+        </template>
+      </el-table-column>
+      <el-table-column label="分析次数" align="center" prop="analysisCount" />
+      <el-table-column label="比赛备注" align="center" prop="matchRemark" />
       <el-table-column label="记录创建者" align="center" prop="createBy" width="90" />
       <el-table-column label="记录创建时间" align="center" prop="createTime" width="160">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.createTime, "{y}-{m}-{d} {h}:{i}:{s}") }}
-          </span>
+          <span>{{ parseTime(scope.row.createTime, "{y}-{m}-{d} {h}:{i}:{s}") }}</span>
         </template>
       </el-table-column>
       <el-table-column label="记录更新者" align="center" prop="updateBy" width="90" />
       <el-table-column label="记录更新时间" align="center" prop="updateTime" width="160">
         <template slot-scope="scope">
-          <span>{{
-            parseTime(scope.row.updateTime, "{y}-{m}-{d} {h}:{i}:{s}")
-            }}</span>
+          <span>{{ parseTime(scope.row.updateTime, "{y}-{m}-{d} {h}:{i}:{s}") }}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right" width="140">
@@ -108,34 +129,73 @@
       @pagination="getList" />
 
     <!-- 添加或修改比赛记录对话框 -->
-    <el-dialog :title="title" :visible.sync="open" :close-on-click-modal="false" width="500px" append-to-body>
+    <el-dialog :title="title" :visible.sync="open" :close-on-click-modal="false" width="800px" style="top: 60px"
+      append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="比赛唯一业务编码" prop="matchCode">
-          <el-input v-model="form.matchCode" placeholder="请输入比赛唯一业务编码" />
-        </el-form-item>
-        <el-form-item label="所属赛季ID" prop="seasonId">
-          <el-input v-model="form.seasonId" placeholder="请输入所属赛季ID" />
-        </el-form-item>
-        <el-form-item label="主队球队ID" prop="homeTeamId">
-          <el-input v-model="form.homeTeamId" placeholder="请输入主队球队ID" />
-        </el-form-item>
-        <el-form-item label="客队球队ID" prop="awayTeamId">
-          <el-input v-model="form.awayTeamId" placeholder="请输入客队球队ID" />
-        </el-form-item>
-        <el-form-item label="比赛开球时间" prop="matchTime">
-          <el-date-picker clearable v-model="form.matchTime" type="date" value-format="yyyy-MM-dd"
-            placeholder="请选择比赛开球时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="比赛举办场地名称" prop="matchVenue">
-          <el-input v-model="form.matchVenue" placeholder="请输入比赛举办场地名称" />
-        </el-form-item>
-        <el-form-item label="比赛业务备注" prop="matchRemark">
-          <el-input v-model="form.matchRemark" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="AI分析次数" prop="analysisCount">
-          <el-input v-model="form.analysisCount" placeholder="请输入AI分析次数" />
-        </el-form-item>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="比赛编码" prop="matchCode">
+              <el-input v-model="form.matchCode" placeholder="请输入比赛编码" :disabled="form.matchId">
+                <template #suffix>
+                  <el-button type="text" icon="el-icon-refresh" @click="generateMatchCode" title="生成比赛编码">
+                    生成
+                  </el-button>
+                </template>
+              </el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="所属赛季" prop="seasonId">
+              <common-enhanced-select ref="seasonSelect" v-model="form.seasonId" valueKey="seasonId"
+                labelKey="seasonName" :api-func="listSeason" placeholder="请选择所属赛季" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="主队名称" prop="homeTeamId">
+              <common-enhanced-select ref="homeTeamSelect" v-model="form.homeTeamId" valueKey="teamId"
+                labelKey="teamName" :api-func="listTeam" placeholder="请选择主队名称" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="客队名称" prop="awayTeamId">
+              <common-enhanced-select ref="awayTeamSelect" v-model="form.awayTeamId" valueKey="teamId"
+                labelKey="teamName" :api-func="listTeam" placeholder="请选择客队名称" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="比赛时间" prop="matchTime">
+              <el-date-picker clearable v-model="form.matchTime" style="width: 100%" type="date"
+                value-format="yyyy-MM-dd" placeholder="请选择比赛时间">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="比赛场地" prop="matchVenue">
+              <el-select v-model="form.matchVenue" style="width: 100%" placeholder="请选择或输入比赛场地" clearable filterable
+                allow-create default-first-option>
+                <el-option v-for="item in teamVenueOptions" :key="item.value" :label="item.label" :value="item.value" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="比赛状态" prop="matchStatus">
+              <el-select v-model="form.matchStatus" style="width: 100%" placeholder="请选择比赛状态">
+                <el-option v-for="dict in dict.type.sys_notice_status" :key="dict.value" :label="dict.label"
+                  :value="dict.value"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="分析次数" prop="analysisCount">
+              <el-input v-model="form.analysisCount" type="number" min="0" max="1023" step="1" placeholder="请输入分析次数" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24">
+            <el-form-item label="比赛备注" prop="matchRemark">
+              <el-input v-model="form.matchRemark" type="textarea" placeholder="请输入内容" />
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -147,10 +207,34 @@
 
 <script>
 import { listMatch, getMatch, delMatch, addMatch, updateMatch } from "@/api/fx67ll/dortmund/match";
+import { listSeason } from "@/api/fx67ll/dortmund/season";
+import { listTeam } from "@/api/fx67ll/dortmund/team";
+
+import { teamVenueOptions } from "@/utils/constant/football";
+
+import CommonEnhancedSelect from "@/components/CommonEnhancedSelect/index";
 
 export default {
   name: "Match",
+  dicts: ["sys_notice_status"],
+  components: { CommonEnhancedSelect },
   data() {
+    // 自定义校验规则：匹配 毫秒级时间戳 + 9位大写字母
+    const validateMatchCode = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('请输入比赛编码'));
+      }
+      // 正则规则：
+      // ^\d+ : 以任意数字开头（毫秒级时间戳，长度13位左右）
+      // [A-Z]{9}$ : 以9位大写字母结尾
+      const reg = /^\d+[A-Z]{9}$/;
+      if (!reg.test(value)) {
+        callback(new Error('比赛编码格式错误，请点击生成按钮修复'));
+      } else {
+        callback();
+      }
+    };
+
     return {
       // 遮罩层
       loading: true,
@@ -174,6 +258,10 @@ export default {
       daterangeCreateTime: [],
       // 更新时间范围
       daterangeUpdateTime: [],
+      // 比赛时间范围
+      daterangeMatchTime: [],
+      // 是否使用高级搜索
+      isMoreQuery: false,
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -184,46 +272,78 @@ export default {
         awayTeamId: null,
         matchTime: null,
         matchVenue: null,
-        matchRemark: null,
         matchStatus: null,
         analysisCount: null,
+        matchRemark: null,
         beginCreateTime: null,
         endCreateTime: null,
         beginUpdateTime: null,
         endUpdateTime: null,
+        beginMatchTime: null,
+        endMatchTime: null,
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
         matchCode: [
-          { required: true, message: "比赛唯一业务编码不能为空", trigger: "blur" }
+          { required: true, message: "比赛编码不能为空", trigger: "blur" },
+          { validator: validateMatchCode, trigger: ['blur', 'change'] }
         ],
         seasonId: [
-          { required: true, message: "所属赛季ID不能为空", trigger: "blur" }
+          { required: true, message: "所属赛季不能为空", trigger: "blur" }
         ],
         homeTeamId: [
-          { required: true, message: "主队球队ID不能为空", trigger: "blur" }
+          { required: true, message: "主队名称不能为空", trigger: "blur" }
         ],
         awayTeamId: [
-          { required: true, message: "客队球队ID不能为空", trigger: "blur" }
+          { required: true, message: "客队名称不能为空", trigger: "blur" }
         ],
         matchTime: [
-          { required: true, message: "比赛开球时间不能为空", trigger: "blur" }
+          { required: true, message: "比赛时间不能为空", trigger: "blur" }
         ],
-      }
+        matchStatus: [
+          { required: true, message: "比赛状态不能为空", trigger: "blur" }
+        ],
+        analysisCount: [
+          { required: true, message: "分析次数不能为空", trigger: "blur" }
+        ],
+      },
+      // 球场预设选项数组
+      teamVenueOptions,
     };
   },
   created() {
+    // 将导入的函数挂载到实例，以便模板中使用
+    this.listSeason = listSeason;
+    this.listTeam = listTeam;
+
     this.getList();
   },
   methods: {
+    // 生成比赛编码：当前timestamp(毫秒级) + 9位随机大写字母
+    generateMatchCode() {
+      // 1. 获取当前毫秒级时间戳（timestamp）
+      const timestamp = Date.now(); // 例如：1741898765432
+
+      // 2. 生成9位随机大写字母（严格保证长度）
+      const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let randomStr = '';
+      for (let i = 0; i < 9; i++) {
+        randomStr += letters[Math.floor(Math.random() * letters.length)];
+      }
+
+      // 3. 拼接时间戳和随机字母，赋值给表单字段
+      this.form.matchCode = timestamp + randomStr;
+    },
     // 重置时间段查询
     clearDateQueryParams() {
       this.queryParams.beginCreateTime = null;
       this.queryParams.endCreateTime = null;
       this.queryParams.beginUpdateTime = null;
       this.queryParams.endUpdateTime = null;
+      this.queryParams.beginMatchTime = null;
+      this.queryParams.endMatchTime = null;
     },
     /** 查询比赛记录列表 */
     getList() {
@@ -236,6 +356,10 @@ export default {
       if (null != this.daterangeUpdateTime && "" != this.daterangeUpdateTime) {
         this.queryParams.beginUpdateTime = this.daterangeUpdateTime[0];
         this.queryParams.endUpdateTime = this.daterangeUpdateTime[1];
+      }
+      if (null != this.daterangeMatchTime && "" != this.daterangeMatchTime) {
+        this.queryParams.beginMatchTime = this.daterangeMatchTime[0];
+        this.queryParams.endMatchTime = this.daterangeMatchTime[1];
       }
       listMatch(this.queryParams).then(response => {
         this.matchList = this.formatObjectArrayNullProperty(response.rows);
@@ -258,15 +382,19 @@ export default {
         awayTeamId: null,
         matchTime: null,
         matchVenue: null,
-        matchRemark: null,
         matchStatus: null,
         analysisCount: null,
+        matchRemark: null,
         createBy: null,
         createTime: null,
         updateBy: null,
         updateTime: null,
       };
       this.resetForm("form");
+    },
+    /** 高级搜索按钮操作 */
+    handleMoreQuery() {
+      this.isMoreQuery = !this.isMoreQuery;
     },
     /** 搜索按钮操作 */
     handleQuery() {
@@ -287,6 +415,7 @@ export default {
     /** 新增按钮操作 */
     handleAdd() {
       this.reset();
+      this.generateMatchCode();
       this.open = true;
       this.title = "添加比赛记录";
     },
