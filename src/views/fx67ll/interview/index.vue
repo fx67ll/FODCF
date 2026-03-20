88c8fd4296fc5f94b15847e06d1e62da6ce987c3
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-03-20 11:26:54 +0800

    add index page

---
+++ src/views/fx67ll/interview/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Interview',
+    data() {
+        return {
+            title: 'Interview'
+        };
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #2ecc71;
+}
+</style>
---
+++ src/views/fx67ll/ip/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IP',
+    data() {
+        return {
+            title: 'IP'
+        };
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #2ecc71;
+}
+</style>
